1ee586e55baff7f8f822d3df7db0bc22c81a18f1
Laura Radville <laura.radville@gmail.com>
2019-06-27 20:01:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-   array.detect { |x| cheese_types.include?(x) }
+  array.detect { |x| cheese_types.include?(x) }
 end
