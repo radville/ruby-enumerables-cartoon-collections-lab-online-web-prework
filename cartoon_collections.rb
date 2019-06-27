4e790482e840a4a04901660153bda30a9f033cd0
Laura Radville <laura.radville@gmail.com>
2019-06-27 19:00:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any do |n|
-    
+  array.any do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
